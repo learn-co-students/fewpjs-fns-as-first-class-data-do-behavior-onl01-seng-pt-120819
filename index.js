abcc5473a5adf02bd2052b7b9665363e369c5347
HarryWilson1995 <harry.wilson1995a@gmail.com>
2020-06-11 06:54:52 -0400

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 /* Given Code, don't edit */
 
-document.querySelector("button").addEventListener("click", handleClick);
+document.querySelector('button').addEventListener('click', handleClick);
 
 function handleClick(e) {
-  const timeString = document.getElementById('time').value
-  displayMessage(greet(timeString))
+  const timeString = document.getElementById('time').value;
+  displayMessage(greet(timeString));
 }
 
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+  const timeOfDay = parseInt(time.split(0, 2));
+  if (timeOfDay < 12) {
+    return 'Good Morning';
+  } else if (timeOfDay >= 12 && timeOfDay <= 17) {
+    return 'Good Afternoon';
+  } else {
+    return 'Good Evening';
+  }
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(string) {
+  document.getElementById('greeting').innerText = string;
+}
